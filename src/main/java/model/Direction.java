fb123c90ae87f8b22c87ad18e31c5813951adfe9
Bagoly Luca Enikő <bagolyluca03@gmail.com>
2024-05-23 15:01:01 +0200

    feat: added Direction enum to model

---
+++ src/main/java/model/Direction.java
@@ -0,0 +1,25 @@
+package model;
+
+public enum Direction {
+    NORTH (-1,0),
+    EAST(0,1),
+    SOUTH(1,0),
+    WEST(0,-1);
+
+    private final int row;
+    private final int col;
+
+    Direction(int row, int col) {
+        this.row=row;
+        this.col=col;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+    
+}
