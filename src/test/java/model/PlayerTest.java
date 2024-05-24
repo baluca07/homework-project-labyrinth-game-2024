f2d1f3dbf0c5d0991df9296859a21ec55e6d648b
Bagoly Luca Enikő <bagolyluca03@gmail.com>
2024-05-24 13:06:45 +0200

    fix: fixed unit tests in PositionTest and PlayerTest, Positions started from (1,1) not from (0,0)

--- src/test/java/model/PlayerTest.java
+++ src/test/java/model/PlayerTest.java
@@ -5,15 +5,15 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PlayerTest {
-    Player player = new Player(Position.of(0, 0));
+    Player player = new Player(Position.of(1, 1));
 
     @Test
     void testToString() {
-        assertEquals("Player is on the position (0,0).", player.toString());
+        assertEquals("Player is on the position (1,1).", player.toString());
     }
 
     @Test
     void getCurrentPosition() {
-        assertEquals(Position.of(0, 0), player.getCurrentPosition());
+        assertEquals(Position.of(1, 1), player.getCurrentPosition());
     }
 }
--- src/test/java/model/PositionTest.java
+++ src/test/java/model/PositionTest.java
@@ -6,52 +6,52 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class PositionTest {
-    Position position = new Position(0, 0);
+    Position position = new Position(1, 1);
 
     @Test
     void of() {
-        assertEquals(new Position(0,0), Position.of(0, 0));
-        assertNotEquals(new Position(0,0), Position.of(1, 1));
+        assertEquals(new Position(1,1), Position.of(1, 1));
+        assertNotEquals(new Position(1,1), Position.of(2, 1));
     }
 
     @Test
     void testToString() {
-        assertEquals("(0,0)", position.toString());
+        assertEquals("(1,1)", position.toString());
     }
 
     @Test
     void testEquals() {
-        assertEquals(Position.of(0, 0),position);
-        assertNotEquals(Position.of(1, 1),position);
+        assertEquals(Position.of(1, 1),position);
+        assertNotEquals(Position.of(2, 1),position);
     }
 
     @Test
     void getRow() {
-        assertEquals(0, position.getRow());
-        assertNotEquals(1, position.getRow());
+        assertEquals(1, position.getRow());
+        assertNotEquals(2, position.getRow());
     }
 
     @Test
     void getCol() {
-        assertEquals(0, position.getCol());
-        assertNotEquals(1, position.getCol());
+        assertEquals(1, position.getCol());
+        assertNotEquals(2, position.getCol());
     }
 
     @Test
     void setRow() {
         Position newPosition = Position.of(position.getRow(), position.getCol());
-        newPosition.setRow(1);
+        newPosition.setRow(3);
         assertNotEquals(position.getRow(), newPosition.getRow());
-        Position positionExpected = new Position(1, 2);
+        Position positionExpected = new Position(3, 2);
         assertEquals(positionExpected.getRow(), newPosition.getRow());
     }
 
     @Test
     void setCol() {
         Position newPosition = Position.of(position.getRow(), position.getCol());
-        newPosition.setCol(1);
+        newPosition.setCol(3);
         assertNotEquals(position.getCol(), newPosition.getCol());
-        Position positionExpected = new Position(2, 1);
+        Position positionExpected = new Position(2, 3);
         assertEquals(positionExpected.getCol(), newPosition.getCol());
     }
 }
