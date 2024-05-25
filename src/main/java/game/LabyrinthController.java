441e2e3f9990fb4575f2cae943c813fe25fcf45e
Bagoly Luca Enikő <bagolyluca03@gmail.com>
2024-05-25 12:07:45 +0200

    feat: added functions to buttons, and to counting steps

--- src/main/java/game/LabyrinthController.java
+++ src/main/java/game/LabyrinthController.java
@@ -2,12 +2,14 @@ package game;
 
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
+import model.Direction;
 import model.LabyrinthState;
 import model.Position;
 import model.Target;
@@ -18,13 +20,15 @@ import static model.LabyrinthState.LABYRINTH_SIZE;
 public class LabyrinthController {
     private StackPane[][] nodes;
     private LabyrinthState state = new LabyrinthState();
+    private int playerStepsTaken;
     @FXML
-    public GridPane labyrinth;
+    private GridPane labyrinth;
     @FXML
-    public Circle playerCircle;
-
+    private Circle playerCircle;
     @FXML
-    public Text targetText;
+    private Text targetText;
+    @FXML
+    private Label stepsLabel;
 
     @FXML
     public void initialize() {
@@ -34,6 +38,8 @@ public class LabyrinthController {
         initializePlayer();
         setPlayerCircle();
         setTargetText();
+        playerStepsTaken = 0;
+        stepsLabel.setText("Steps: " + playerStepsTaken);
     }
 
     private void initializeGrid() {
@@ -56,15 +62,16 @@ public class LabyrinthController {
         labyrinth.getStyleClass().add("basicBorder");
         Logger.info("Initialized grid");
     }
+
     public void setWalls() {
         for (int i = 0; i < LABYRINTH_SIZE; i++) {
             for (int j = 0; j < LABYRINTH_SIZE; j++) {
-                var labyrinthCell = state.getLabyrinthCellAtPosition(Position.of(i+1,j+1));
+                var labyrinthCell = state.getLabyrinthCellAtPosition(Position.of(i + 1, j + 1));
                 var stackPane = nodes[i][j];
                 stackPane.getStyleClass().addAll("topBorder", "rightBorder", "bottomBorder", "leftBorder");
-                for(var direction : labyrinthCell.getDirectionCanGo()){
-                    switch (direction){
-                        case NORTH ->  stackPane.getStyleClass().remove("topBorder");
+                for (var direction : labyrinthCell.getDirectionCanGo()) {
+                    switch (direction) {
+                        case NORTH -> stackPane.getStyleClass().remove("topBorder");
                         case EAST -> stackPane.getStyleClass().remove("rightBorder");
                         case SOUTH -> stackPane.getStyleClass().remove("bottomBorder");
                         case WEST -> stackPane.getStyleClass().remove("leftBorder");
@@ -74,6 +81,7 @@ public class LabyrinthController {
         }
         Logger.info("Walls set up on GUI");
     }
+
     @FXML
     public void setOuterWalls() {
         for (int i = 0; i < LABYRINTH_SIZE; i++) {
@@ -84,26 +92,66 @@ public class LabyrinthController {
         }
         Logger.info("Outer walls set up on GUI");
     }
-    private void initializePlayer(){
+
+    private void initializePlayer() {
         playerCircle = new Circle(16, Color.BLUEVIOLET);
         playerCircle.setStroke(Color.BLACK);
         labyrinth.add(playerCircle, 0, 0);
         GridPane.setHalignment(playerCircle, Pos.CENTER.getHpos());
         Logger.info("PLayer initialized");
     }
+
     public void setPlayerCircle() {
         Position currentPosition = state.getPlayer().getCurrentPosition();
         GridPane.setRowIndex(playerCircle, currentPosition.getRow() - 1);
         GridPane.setColumnIndex(playerCircle, currentPosition.getCol() - 1);
         Logger.info("Player position set on GUI");
     }
+
     @FXML
     public void setTargetText() {
         targetText = new Text("CÉL");
         targetText.getStyleClass().add("targetText");
-        labyrinth.add(targetText, Target.getPosition().getCol() - 1,Target.getPosition().getRow() - 1);
+        labyrinth.add(targetText, Target.getPosition().getCol() - 1, Target.getPosition().getRow() - 1);
         GridPane.setHalignment(targetText, Pos.CENTER.getHpos());
         Logger.info("Target text set on GUI");
     }
+
+    public void movePlayer(Direction direction) {
+        if (!state.isSolved()) {
+            playerStepsTaken++;
+        }
+        state.makeMove(direction);
+        Logger.info("Player moved to " + direction);
+        setPlayerCircle();
+        stepsLabel.setText("Steps: " + playerStepsTaken);
+        Logger.trace(state.getPlayer().toString());
+        Logger.info("Steps: {}", playerStepsTaken);
+    }
+
+    @FXML
+    public void goNorth() {
+        if (state.isLegalMove(Direction.NORTH))
+            movePlayer(Direction.NORTH);
+    }
+
+    @FXML
+    public void goSouth() {
+        if (state.isLegalMove(Direction.SOUTH))
+            movePlayer(Direction.SOUTH);
+    }
+
+    @FXML
+    public void goEast() {
+        if (state.isLegalMove(Direction.EAST))
+            movePlayer(Direction.EAST);
+    }
+
+    @FXML
+    public void goWest() {
+        if (state.isLegalMove(Direction.WEST))
+            movePlayer(Direction.WEST);
+    }
+
 
 }
